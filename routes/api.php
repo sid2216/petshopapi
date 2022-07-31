ecb3e53d55fc9b034745b2168ab12de3061ac806
siddheshd22 <siddhu.deorukhkar@gmail.com>
2022-08-01 00:52:38 +0530

    add routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,45 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//admin routes
+Route::post('api/v1/admin/create','AdminController@create');
+Route::post('api/v1/admin/login','AdminController@login');
+Route::post('api/v1/admin/logout','AdminController@logout');
+Route::get('api/v1/admin/user-listing','AdminController@user_listing');
+Route::put('api/v1/admin/user-edit/{uuid}','AdminController@edit_user');
+Route::delete('api/v1/admin/user-delete/{uuid}','AdminController@destroy');
+//user routes
+Rout::get('api/v1/user','UserController@show');
+Route::delete('api/v1/user','UserController@destroy');
+Route::get('api/v1/user/orders','UserController@_user_orders');
+Route::post('api/v1/user/create','UserController@create');
+Route::post('api/v1/user/forgot-password','UserController@forget_password');
+Route::post('api/v1/user/login','UserController@login');
+Route::post('api/v1/user/logout','UserController@logout');
+Route::post('api/v1/user/reset-password-token','UserController@reset_password_token');
+Route::put('api/v1/user/edit','UserController@edit_user');
+//category route
+Route::get('api/v1/categories','CategoryController@category_index');
+Route::post('api/v1/category/create','CategoryController@create');
+Route::put('api/v1/category/{uuid}','CategoryController@edit_category');
+Route::get('api/v1/category/{uuid}','CategoryController@category_show');
+Route::delete('api/v1/category/{uuid}','CategoryController@delete_category');
+//Main Page route
+Route::get('api/v1/main/blog','MainPageController@index_promotions');
+Route::get('api/v1/main/blog/{uuid}','MainPageController@index_blog');
+Route::get(' api/v1/main/promotions','MainPageController@show_post');
+//product route 
+Route::post('api/v1/product/create','ProductController@create');
+Route::put('api/v1/product/{uuid}','ProductController@edit_product');
+Route::get('api/v1/product/{uuid}','ProductController@product_show');
+Route::delete('api/v1/product/{uuid}','ProductController@delete_product');
+Route::get('api/v1/products','ProductController@category_index');
+//order route
+//brand route
+Route::get('api/v1/brands','BrandController@brand_index');
+Route::post('api/v1/brand/create','BrandController@create');
+Route::put('api/v1/brand/{uuid}','BrandController@edit_brand');
+Route::get('api/v1/brand/{uuid}','BrandController@brand_show');
+Route::delete('api/v1/brand/{uuid}','BrandController@delete_brand');
+//
